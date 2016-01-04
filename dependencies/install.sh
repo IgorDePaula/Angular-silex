428a92ec74ddf2dc13ec8fa0606a0ff3f5389432
Igor C. de Paula <principe.borodin@gmail.com>
2016-01-04 16:29:17 -0200

    enable mod reqrite on apache

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -2,6 +2,7 @@
 apt-get update
 apt-get install apache2 php5 libapache2-mod-php5 gcc g++ make git git-core clang-3.4 clang++-3.4 -y 
 apt-get install build-essential libssl-dev -y
+a2enmod rewrite
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 curl -sL https://deb.nodesource.com/setup_4.x | bash -
