81b3504846739db44da3b930bc31bfd7ac788b77
Igor De Paula <principe.borodin@gmail.com>
2016-01-09 12:55:57 -0200

    Add files to test build

---
+++ src/PJenkins/Math/Calc.php
@@ -0,0 +1,18 @@
+<?php
+namespace PJenkins\Math;
+
+/**
+ * Class for Calcs
+ */
+class Calc
+{
+    /**
+     * Sum 2 integers
+     * @param Integer $a An integer
+     * @param Integer $b An integer
+     */
+    public function sum($a, $b)
+    {
+        return $a + $b;
+    }
+}
